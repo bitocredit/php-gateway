7151ca1695ccc21be6aedf75524dfd0b748cd60a
reza motahary <56221733+rezaMotahary@users.noreply.github.com>
2022-08-17 02:45:50 -0700

    Update config.sample.php

--- src/config.sample.php
+++ src/config.sample.php
@@ -4,16 +4,6 @@
 $baseURL = "https://bitocredit.com/api/";
 $token = "";
 
-// for unittest
-$baseURL_test = "https://bitocredit.com/api/";
-$token_test = "";
-$transaction_test = "";
-$wallet_address_test = "";
-
 
 define("baseURL", $baseURL);
 define("token", $token);
-define("baseURL_test", $baseURL_test);
-define("token_test", $token_test);
-define("transaction_test", $transaction_test);
-define("wallet_address_test", $wallet_address_test);
